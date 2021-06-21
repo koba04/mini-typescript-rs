a375d591ef3bceb33199016a454323186fdc03ed
Toru Kobayashi <koba0004@gmail.com>
2021-06-22 00:08:36 +0900

    feat: add base statements

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,56 @@
+enum Token {
+    Function,
+    Var,
+    Return,
+    Equals,
+    Literal,
+    Identifier,
+    Newline,
+    Semicolon,
+    Colon,
+    Whitespace,
+    Unknown,
+    BOF,
+    EOF,
+}
+
+struct Lexer {
+}
+
+
+fn lex(code: String) -> Lexer {
+    Lexer {}
+
+}
+
+fn bind(tree: &Lexer) {
+
+}
+
+fn check(tree: &Lexer) {
+
+}
+
+fn emit(tree: &Lexer) {
+}
+
+fn transform(tree: &Lexer) -> &Lexer {
+    tree
+}
+
+pub fn compile(code: String) {
+    let tree = lex(code);
+    bind(&tree);
+    check(&tree);
+    emit(transform(&tree))
+}
+
 #[cfg(test)]
 mod tests {
+    use super::compile;
     #[test]
     fn it_works() {
+        compile(String::from("var num: number = 1"));
         assert_eq!(2 + 2, 4);
     }
 }
