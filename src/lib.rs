75a2cc16442d058b625472700a40f115b5decd98
Toru Kobayashi <koba0004@gmail.com>
2021-06-29 00:49:13 +0900

    feat(wip): implement the scan method (a test is failing)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
 use regex::Regex;
 
+#[derive(Copy, Clone)]
 enum Token {
     Function,
     Var,
@@ -20,19 +22,48 @@ struct Lexer {
     pos: usize,
     token: Token,
     text: String,
-    original_text: String
+    original_text: String,
+    keyword_map: HashMap<String, Token>,
 }
+
 impl Lexer {
     fn new(str: String) -> Lexer {
+        let mut keyword_map = HashMap::new();
+        keyword_map.insert(String::from("function"), Token::Function);
+        keyword_map.insert(String::from("var"), Token::Var);
+        keyword_map.insert(String::from("return"), Token::Return);
+
         Lexer {
             pos: 0,
             token: Token::BOF,
             text: String::from(""),
-            original_text: str
+            original_text: str,
+            keyword_map,
         }
     }
     fn scan(&mut self) {
-        self.scan_forward(Regex::new(r"[ \t\n]").unwrap())
+        self.scan_forward(Regex::new(r"[ \t\n]").unwrap());
+        let start = self.pos;
+        if self.pos == self.original_text.len() {
+            self.token = Token::EOF;
+        } else if Regex::new(r"[0-9]").unwrap().is_match(&self.original_text.chars().nth(self.pos).unwrap().to_string()) {
+            self.scan_forward(Regex::new(r"[0-9]").unwrap());
+            self.text = self.original_text[start..self.pos].to_string();
+            if let Some(k) = self.keyword_map.get(&self.text) {
+                self.token = *k;
+            } else {
+                self.token = Token::Identifier;
+            }
+        } else {
+            self.pos += 1;
+            self.token = match self.original_text.chars().nth(self.pos - 1) {
+                Some('=') => Token::Equals,
+                Some(';') => Token::Semicolon,
+                Some(':') => Token::Colon,
+                _   => Token::Unknown
+            }
+        }
+
     }
     fn scan_forward(&mut self, regex: Regex) {
         while self.pos < self.original_text.len() {
@@ -77,7 +108,7 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
     #[test]
-    fn scan() {
+    fn scan_forward() {
         let mut lex = Lexer::new(String::from("var a = 1"));
         lex.scan();
         assert_eq!(lex.pos, 0);
